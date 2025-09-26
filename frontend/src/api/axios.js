a173ac2f736d4ad9be1a749e32be5c5822a47bfc
Amit sharma <sharmaamit9627172414@gmail.com>
2025-09-26 23:03:05 +0530

    commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 // src/api/axios.js
 import axios from "axios";
 
-// Create an axios instance
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
